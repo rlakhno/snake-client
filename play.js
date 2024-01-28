6b1e96bbef0ecb14aeec0cc441b150e0e0398ea5
Rouslan Lakhno <rouslanlakhno@gmail.com>
2024-01-28 17:03:49 -0500

    Used Shorthand

--- play.js
+++ play.js
@@ -1,21 +1,5 @@
-const net = require("net");
+const {connect} = require('./client');
 
-// establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: "172.19.179.140",
-    port: 50541
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  
-  conn.on("data", (data) => {
-    console.log("data: ", data);
-  });
-
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
